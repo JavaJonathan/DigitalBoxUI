8b32bf6a368e9ac605d1c51d03af596bce1eff63
JavaJonathan <jonathan.henderson24@yahoo.com>
2025-12-29 11:03:39 -0500

    code clean up

--- digital-box/src/Components/NoteModal.js
+++ digital-box/src/Components/NoteModal.js
@@ -1,7 +1,6 @@
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
